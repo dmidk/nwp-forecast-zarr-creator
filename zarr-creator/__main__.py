2738f79c11b4ef4d518d131ffed74fd65fb215f0
EC2 Default User <ec2-user@ip-172-31-26-204.eu-central-1.compute.internal>
2025-02-26 14:33:25 +0000

    working zarr write and cleanup

--- zarr-creator/__main__.py
+++ zarr-creator/__main__.py
@@ -1,19 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 import sys
 from loguru import logger
+import argparse
 
-from .arguments import arguments
-from .run import run
+from .read_source import read_source
+from .write_zarr import write_zarr
 
-def cli():
+DEFAULT_ANALYSIS_TIME = "2025-02-17T01:00:00Z"
+DEFAULT_FORECAST_DURATION = "PT3H"
+DEFAULT_CHUNKING = dict(time=54, x=300, y=260)
+
+def _setup_argparse():
+    argparser = argparse.ArgumentParser(
+        description="Create Zarr dataset from data-catalog (dmidc)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    argparser.add_argument(
+        "--analysis_time",
+        default=DEFAULT_ANALYSIS_TIME,
+        help="Analysis time as ISO8601 string",
+    )
+
+    argparser.add_argument(
+        "--forecast_duration",
+        default=DEFAULT_FORECAST_DURATION,
+        help="Forecast duration as ISO8601 duration",
+    )
+
+    argparser.add_argument(
+        "--verbose", 
+        action="store_true", 
+        help="Verbose output", 
+        default=False
+    )
+
+    argparser.add_argument(
+        "--source",
+        type=str,
+        help="Which data-catalog source to use",
+        default="dinisf",
+        required=False,
+    )
+
+    argparser.add_argument(
+        "--output_path",
+        type=str,
+        help="Output path for zarr dataset",
+        default="{source}_{analysis_time}_{forecast_duration}.zarr",
+        required=False,
+    )
+
+    argparser.add_argument(
+        "--log-level", default="INFO", help="The log level to use"
+    )
+
+    argparser.add_argument(
+        "--log-file", default=None, help="The file to log to"
+    )
+    
+    return argparser
+
+def cli(argv=None):
     """
     Run zarr creator
     """
-
-    modargs = arguments()
-    args = modargs.get_args(sys.argv)
+    
+    argparser = _setup_argparse()
+    args = argparser.parse_args(argv)
 
     logger.remove()
     logger.add(sys.stderr, level=args.log_level.upper())
@@ -22,8 +77,18 @@ def cli():
 
     if args.source not in ["dinisf"]:
         raise ValueError("Invalid data-catalog source: {}".format(args.source))
+    
+    fp_out = args.output_path.format(
+        source=args.source,
+        analysis_time=args.analysis_time,
+        forecast_duration=args.forecast_duration
+    )
 
-    run(args)
+    logger.info("Creating zarr dataset from source: {}".format(args.source))
+
+    ds = read_source(source_name=args.source, t_analysis=args.analysis_time, forecast_duration=args.forecast_duration)
+
+    write_zarr(ds=ds, fp_out=fp_out, rechunk_to=DEFAULT_CHUNKING)
     
 
 if __name__ == "__main__":
--- zarr-creator/write_zarr.py
+++ zarr-creator/write_zarr.py
@@ -13,7 +13,7 @@ import shutil
 import tempfile
 
 
-def write_zarr(ds, fp_out, overwrite=True, temp_dir=None):
+def write_zarr(ds, fp_out, rechunk_to, overwrite=True, temp_dir=None):
     """
     Write a xarray dataset to a zarr store.
 
@@ -21,16 +21,25 @@ def write_zarr(ds, fp_out, overwrite=True, temp_dir=None):
     ----------
     ds : xarray.Dataset
         The dataset to write.
-
     fp_out : Path
         The path to the zarr store to create.
+    rechunk_to : dict
+        A dictionary specifying the target chunk size for each dimension.
+        Only the dimensions that are present in the dataset will be used, and
+        the size limited to the size of the dimension (if the chunk size
+        provided is larger).
+    overwrite : bool
+        Whether to overwrite the zarr store if it already exists.
+    temp_dir : Path
+        The temporary directory to use for intermediate files. If None, a
+        temporary directory will be created.
     """
+    fp_out = Path(fp_out)
 
     if not fp_out.name.endswith(".zarr"):
         raise Exception(f"fp_out must end with .zarr, got {fp_out}")
 
-    zarr_filepath = fp_out
-    zarr_filepath.parent.mkdir(parents=True, exist_ok=True)
+    fp_out.parent.mkdir(parents=True, exist_ok=True)
     
     if Path(fp_out).exists():
         if overwrite:
@@ -38,13 +47,8 @@ def write_zarr(ds, fp_out, overwrite=True, temp_dir=None):
         else:
             logger.error(f"{fp_out} already exists. Set overwrite=True to overwrite.")
 
-
-
     fs = fsspec.filesystem("file")
     mapper = fs.get_mapper(fp_out)
-
-
-    rechunk_to = dict(time=54, x=300, y=260)
 
     for d in ds.dims:
         dim_len = len(ds[d])
@@ -71,83 +75,13 @@ def write_zarr(ds, fp_out, overwrite=True, temp_dir=None):
     if temp_dir is None:
         temp_dir = Path(tempfile.TemporaryDirectory().name)
 
-    # print(ds)
-    # print(rechunk_to)
-    print(target_chunks)
-
-    # remove chunks encoding
-    for v in ds.data_vars:
-        if "chunks" in ds[v].encoding:
-            del ds[v].encoding["chunks"]
-            # del ds[v].encoding["compressor"]
-            # del ds[v].encoding["preferred_chunks"]
-            # del ds[v].encoding["filters"]
-            print(ds[v].encoding)
-            print(f"removed chunks encoding for {v}")
-   
-    del ds.x.encoding["chunks"]
-    del ds.y.encoding["chunks"]
-    del ds.time.encoding["chunks"]
-    del ds.level.encoding["chunks"]
-    del ds.lat.encoding["chunks"]
-    del ds.lon.encoding["chunks"]
-
-    # del ds.x.encoding["preferred_chunks"]
-    # del ds.y.encoding["preferred_chunks"]
-    # del ds.time.encoding["preferred_chunks"]
-    # del ds.level.encoding["preferred_chunks"]
-    # del ds.lat.encoding["preferred_chunks"]
-    # del ds.lon.encoding["preferred_chunks"]
-
-    # del ds.x.encoding["compressor"]
-    # del ds.y.encoding["compressor"]
-    # del ds.time.encoding["compressor"]
-    # del ds.level.encoding["compressor"]
-    # del ds.lat.encoding["compressor"]
-    # del ds.lon.encoding["compressor"]
-
-    # del ds.x.encoding["filters"]
-    # del ds.y.encoding["filters"]
-    # del ds.time.encoding["filters"]
-    # del ds.level.encoding["filters"] 
-    # del ds.lat.encoding["filters"]
-    # del ds.lon.encoding["filters"]
-
-    print(ds)
-    print(ds.x.encoding)
-    print(f"Type of ds: {type(ds)}")  # Should be <xarray.Dataset>
-    print(f"Type of fp_out: {type(fp_out)}")  # Should be <pathlib.Path>
-    print(f"fp_out: {fp_out}")  # Check if it’s a valid path
-    print(f"Target Chunks: {target_chunks}")  # Debug output
-
-    print(f"Type of target_store: {type(target_store)}")
-    
+    # reset the encoding so that the zarr dataset that is written isn't written
+    # with an encoding that is reliant on the gribscan package's decoding
+    # functions
     ds.encoding = {}
+    for var_name in ds.data_vars:
+        ds[var_name].encoding = {}
     ds.to_zarr(target_store, mode="w", compute=True, consolidated=True)
-
-
-    # try:
-    #     r = rechunker.rechunk(
-    #         ds,
-    #         target_chunks=target_chunks,
-    #         max_mem="1GB",
-    #         target_store=target_store, # open("/dmidata/scratch/10day/kah/dini2.zarr","wb"),
-    #         temp_store=temp_dir,
-    #     )
-
-    #     logger.info(f"writing to {fp_out}")
-    #     print(f"Type of ds: {type(ds)}")  # Should be <xarray.Dataset>
-    #     print(f"Type of target_store: {type(target_store)}")  # Should be a Zarr store, not a string
-    #     print(f"Type of temp_store: {type(temp_dir)}")  # Should be <pathlib.Path>
-    #     print(f"Target store path: {target_store}")  # Check if it's a valid Zarr store
-    #     print(f"Temp store path: {temp_dir}")  # Check if temp directory exists
-
-    #     r.execute()
-    #     logger.info(r)
-    # finally:
-    #     shutil.rmtree(temp_dir)
-
-    # consolidate_metadata(mapper)
 
     logger.info("done!", flush=True)
 
