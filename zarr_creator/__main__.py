0f0eaceb2faae00fe1d92239b229e61772ac40cd
Leif Denby <leif@denby.eu>
2025-09-29 17:04:07 +0200

    set zarr-creator version in output

--- zarr_creator/__main__.py
+++ zarr_creator/__main__.py
@@ -10,6 +10,7 @@ from loguru import logger
 from .config import DATA_COLLECTION
 from .read_source import read_level_type_data
 from .write_zarr import write_zarr_to_s3
+from . import __version__
 
 DEFAULT_ANALYSIS_TIME = "2025-02-17T01:00:00Z"
 DEFAULT_FORECAST_DURATION = "PT3H"
@@ -112,6 +113,9 @@ def cli(argv=None):
         # check that with the chunking provided that the arrays exactly fit into the chunks
         for dim in rechunk_to:
             assert ds_part[dim].size % rechunk_to[dim] == 0
+            
+        # set zarr-creator version
+        ds_part.attrs["zarr_creator_version"] = __version__
 
         write_zarr_to_s3(
             ds=ds_part,
