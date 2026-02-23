1c4937e0407d6796c3a2fbe63c001b2612852bee
Leif Denby <leif@denby.eu>
2026-02-23 10:08:05 +0100

    remove unnecessary merging split named fields (u10, u100, u...)

--- zarr_creator/read_source.py
+++ zarr_creator/read_source.py
@@ -64,9 +64,6 @@ def read_level_type_data(t_analysis: datetime.datetime, level_type: str) -> xr.D
             ds[var_name].attrs["units"] = "J m-2"
 
     if level_type == "heightAboveGround":
-        # u-wind @ 10m and 100m are given as their own variables... same for v-wind
-        # ds = _merge_special_fields(ds)
-
         # land-sea mask is given for each timestep even though it doesn't
         # change, let's remove the time dimension
         ds["lsm"] = ds.isel(time=0).lsm
@@ -159,67 +156,6 @@ def _add_projection_info(ds):
 
     for var_name in ds.data_vars:
         ds[var_name].attrs["grid_mapping"] = PROJECTION_IDENTIFIER
-
-
-def _merge_special_fields(ds):
-    # u-wind component in general has paramter-id 131, but at 10m altitude is
-    # parameter 165 and at 100m altitude is parameter 228246:
-    #    shortName   paramId     level
-    #    10u         165         10
-    #    u           131         50
-    #    100u        228246      100
-    #    u           131         150
-    #    u           131         250
-    #    u           131         350
-    #    u           131         450
-    # same for v-wind component (in general paramId 132, 166 @ 10m, 228247 @ 100m):
-    #    shortName   paramId     level
-    #    10v         166         10
-    #    v           132         50
-    #    100v        228247      100
-    #    v           132         150
-    #    v           132         250
-    #    v           132         350
-    #    v           132         450
-
-    # this means that `u` and `v` actually have nan values at 10m and 100m
-    # altitudes, which we should replace with the values from `10u` and `10v`
-    # and `100u` and `100v` respectively
-
-    ds_copy = ds.copy()
-
-    special_params = {
-        "u": {
-            "10u": 10,
-            "100u": 100,
-        },
-        "v": {
-            "10v": 10,
-            "100v": 100,
-        },
-        "t": {
-            "2t": 2,
-        },
-    }
-
-    data_arrays = {}
-    for true_param, special_param_info in special_params.items():
-        da = ds[true_param]
-
-        for special_param, level in special_param_info.items():
-            keep_levels = [lev for lev in da.level.values if lev != level]
-            da_subset = da.sel(level=keep_levels)
-
-            da_special = ds[special_param]
-            da_special["level"] = level
-
-            da = xr.concat([da_subset, da_special], dim="level")
-        data_arrays[true_param] = da
-
-    ds_copy = xr.merge(
-        list(data_arrays.values()) + [ds.drop_vars(list(special_params.keys()))]
-    )
-    return ds_copy
 
 
 def merge_level_specific_params(ds, true_param, level, short_name):
